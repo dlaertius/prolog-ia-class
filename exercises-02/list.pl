5ef5053b86d55280ff0f53f132b147588cd60563
Diogenes Laertius <dlaertius>
2017-10-04 13:30:59 -0300

    List updated

--- exercises-02/list.pl
+++ exercises-02/list.pl
@@ -6,3 +6,19 @@ member(X, [_|Y]) :- member(X,Y).
 /*Add X object in head position of new list.*/
 add(X,L,[X|L]).
 
+/*Add at the end*/
+    add(X,[],[X]).
+    
+    add(X,[Y|Tail],[Y|Tail1]):-
+        add(X,Tail,Tail1).
+
+/*concatenation*/
+conc([], L, L).
+
+/*in the beginning*/
+conc([X|L1], L2, [X|L3]) :- conc(L1,L2,L3).
+
+/*Sum*/
+sum_list([], 0).
+/*Pay attention on the difference between 'is' and '=' arithmetic x structure operator.*/
+sum_list([H|T], Sum) :- sum_list(T, Rest), Sum is H + Rest. 
